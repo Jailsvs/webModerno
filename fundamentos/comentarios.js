03e6f9ec2b5d6af766de8aff0b55c55db0dc628f
Carlos Eduardo <carlos_dbs@hotmail.com>
2018-09-06 19:03:38 -0300

    Adicionando comentários no código

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,14 @@
+//Comentário de uma linha única
+console.log("Hello World!"); //imprimindo um hello World
+
+/*
+    Comentário
+    de multiplas
+    linhas
+ */
+
+var nome = "Carlos Eduardo"; //Criando uma variável String
+var idade = 26; //Criando uma variável inteira
+
+console.log(nome);
+console.log(idade);
