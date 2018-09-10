4553efdfad81ed10ecaa6dde2a31b6dbbd5f08a7
Carlos Eduardo <carlos_dbs@hotmail.com>
2018-09-10 20:24:18 -0300

    Trabalhando tipos númericos e funções

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+//Função typeof informa qual o tipo da variavel criada.
+console.log(typeof(qualquer));
+
+qualquer = 3.1618;
+console.log(qualquer);
+console.log(typeof(qualquer));
+
+qualquer = false;
+console.log(qualquer);
+console.log(typeof qualquer);
