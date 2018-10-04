4fb3b5407690eafa6a8ec9218bed1fc2d5415609
Carlos Eduardo B Souza <carlos_dbs@hotmail.com>
2018-10-04 14:45:45 -0300

    Finalizando aulas de Switch

--- controle/switch.js
+++ controle/switch.js
@@ -1,33 +1,46 @@
-const imprimirResultado = function(nota)
-{
-    switch(nota)
-    {
+const imprimirResultado = function (nota) {
+    switch (nota) {
         case 0:
-        {
-            console.log("Reprovado");
-            break;
-        }
+            {
+                console.log("Reprovado");
+                break;
+            }
         case 5:
-        {
-            console.log("Recuperacão");
-            break;
-        }
+            {
+                console.log("Recuperacão");
+                break;
+            }
 
         case 10:
-        {
-            console.log("Aprovado");
-            break;
-        }
+            {
+                console.log("Aprovado");
+                break;
+            }
         default:
-        {
-            console.log("Nota inválida! ");
-            break;
-        }
+            {
+                console.log("Nota inválida! ");
+                break;
+            }
     }
     console.log("FIM");
 }
 
-imprimirResultado(0);
-imprimirResultado(5);
-imprimirResultado(10);
-imprimirResultado(12);
+const nome = 'Carlos';
+console.log(nome);
+
+// nome = 'Gabriel';
+
+// imprimirResultado(0);
+// imprimirResultado(10);
+// imprimirResultado(12);
+// imprimirResultado(5);
+let nota = 8;
+
+if (nota > 7) {
+    console.log('hushs');
+}
+
+
+{
+    console.log(nota);
+}
