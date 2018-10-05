c3849432da1c16a36956e052afcb32a97b4ecdce
Carlos Eduardo de Barros Souza <40256701865@info.local>
2018-10-05 16:31:57 -0300

    Renomeando pastas e iniciando functions

---
+++ 3_funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,18 @@
+//Funões em Javascrit são First-Class Object (Citizens)
+//Higher Order function
+
+/**
+ * Isso quer dizer que JS trabalha função como dado
+ */
+
+ //Criando uma função de forma literal ou FUNCTION DECLARATION
+ function helloWorld()
+{
+
+}
+
+//Pode-se armazenar uma function em uma variavel FUNCTION EXPRESSION
+let imprimirDados = function()
+{
+
+}
