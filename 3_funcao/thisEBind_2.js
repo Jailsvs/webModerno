a296febbf042adcc630e2eaa05b92d859b103885
Carlos Eduardo B Souza <carlos_dbs@hotmail.com>
2018-10-08 16:14:39 -0300

    Uso do this

---
+++ 3_funcao/thisEBind_2.js
@@ -0,0 +1,21 @@
+function Pessoa(){
+    this.idade = 0;
+
+    //Cambiarra do JS
+    const self = this;
+    //Dispara uma determinada acao em um tempo determinado em milisegundo
+    setInterval(function()
+    {
+        self.idade++;
+        console.log(self.idade);
+    }/*.bind(this)*/, 1000);
+}
+
+new Pessoa();
+
+
+
+
+
+
+
