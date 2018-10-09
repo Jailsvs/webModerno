4391be5011307199acfc29f0b028992a9e94c3ce
Carlos Eduardo B Souza <carlos_dbs@hotmail.com>
2018-10-09 16:31:37 -0300

    Callback

---
+++ 3_funcao/callBack_1.js
@@ -0,0 +1,9 @@
+const fabricantes = ['Mercedes', 'volkswagen', 'Fiat', 'Toyota', 'Chevrolet'];
+
+function imprimir(nome, indice)
+{
+    console.log(`${indice + 1}.${ nome }`);
+    
+}
+
+imprimir()
