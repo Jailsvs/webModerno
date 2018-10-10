55387dc784bc42690f9e39b4463c46c243ae3d56
Carlos Eduardo de Barros Souza <40256701865@info.local>
2018-10-10 16:43:11 -0300

    Resumo de declaracao de funcao

---
+++ 3_funcao/tiposDeclaracao.js
@@ -0,0 +1,27 @@
+/**
+ * Functions Declaration são as unicas que podem ser invocadas antes de sua declaracao 
+ */
+console.log(soma(3,4));
+
+//function declaration
+function soma(x, y)
+{
+    return x + y;
+}
+
+//function expression
+const sub = function(x, y)
+{
+    return x - y;
+}
+
+console.log(sub(3, 4));
+
+
+//named function expression
+const mult = function mult(x, y)
+{
+    return x * y;
+}
+
+console.log(mult(3,4));
