8ce8e741becb0e563683344d881a55ffb953b7f4
carlosDbs <carlos_dbs@hotmail.com>
2018-10-16 16:28:46 -0300

    Conceitos e  OO

---
+++ 4_objeto/oo.js
@@ -0,0 +1,39 @@
+/**
+ * Os códigos feitos nessa aula não seram códigos executaveis mas sim códigos
+ * para exemplificar o paradigma de programacao orientado a objetos
+ */
+
+ //Programação Procedural
+ /**
+  * Conceito mais voltado para os procedimentos/funcoes que manipulam dados 
+  * globais ou locais
+  */
+ processamento(valor_1, valor_2, valor_3)
+ {
+
+ }
+
+ /**
+  * OO Busca encapsular os dados em objetos para que esses possam simular situações da
+  * vida real
+  */
+ objeto = {
+     valor_1,
+     valor_2,
+     valor_3,
+
+     processamento(){
+         //codigo ...
+     }
+ }
+
+ objeto.processamento();
+
+ //Príncipos de Orientação a Objetos
+
+ /**
+  * 1 - Abstracao
+  * 2 - Encapsulamento
+  * 3 - Herança - prototype
+  * 4 - Polimorfismo
+  */
