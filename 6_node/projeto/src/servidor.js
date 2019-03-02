2699e60b9632a20dc04b726e965481ea16d021ab
Carlos Eduardo <carlos_dbs@hotmail.com>
2019-03-02 14:24:11 -0300

    Simulando Banco De Dados com JS

--- 6_node/projeto/src/servidor.js
+++ 6_node/projeto/src/servidor.js
@@ -1,5 +1,7 @@
 //Porta para comunicação do servidor web
 const porta = 3003;
+//Importando o modulo bancoDeDados criado
+const bancoDeDados = require('./bancoDeDados');
 
 //importando express no modulo atual
 const express = require('express');
@@ -8,8 +10,21 @@ const express = require('express');
 const app = express();
 
 app.get('/produtos', (request, response, next) => {
-    response.send({ nome: 'Notebook', preco: 123.45 }) //Esse objeto será convertido para JSON automaticamente
+    response.send(bancoDeDados.getProdutos()) //Esse objeto será convertido para JSON automaticamente
 });
+
+//:id é um parametro
+app.get('/produtos/:id', (request, response, next) => {
+    response.send(bancoDeDados.getProduto(request.params.id))
+});
+
+app.post('/produtos', (request, response, next) => {
+    const produto = bancoDeDados.salvarProdutos({
+        nome: request.body.name,
+        preco: request.body.preco
+    });
+    response.send(produto); //JSON
+})
 
 //Seta a porta em que a aplicação ficará escutando para receber e enviar suas requisicoes.
 app.listen(porta, () => {
